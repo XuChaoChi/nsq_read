eb393484d1081b193a277cddb0096fc7797f9370
xuchaochi <xuchaochi@gmail.com>
2019-09-09 02:18:21 +0800

    [up]提交日志阅读

--- internal/lg/lg.go
+++ internal/lg/lg.go
@@ -1,3 +1,4 @@
+//nsq对标准库日志的简单封装
 // short for "log"
 package lg
 
@@ -8,6 +9,7 @@ import (
 	"strings"
 )
 
+//日志等级
 const (
 	DEBUG = LogLevel(1)
 	INFO  = LogLevel(2)
@@ -16,22 +18,29 @@ const (
 	FATAL = LogLevel(5)
 )
 
+//定义用者使用日志函数的标准
 type AppLogFunc func(lvl LogLevel, f string, args ...interface{})
 
+//日志接口
 type Logger interface {
 	Output(maxdepth int, s string) error
 }
 
+//定义一个空的日志
 type NilLogger struct{}
 
+//空日志必须实现的接口
 func (l NilLogger) Output(maxdepth int, s string) error {
 	return nil
 }
 
+//定义日志等级类型
 type LogLevel int
 
+//获取日志等级
 func (l *LogLevel) Get() interface{} { return *l }
 
+//设置日志等级
 func (l *LogLevel) Set(s string) error {
 	lvl, err := ParseLogLevel(s)
 	if err != nil {
@@ -41,6 +50,7 @@ func (l *LogLevel) Set(s string) error {
 	return nil
 }
 
+//获取当前日志等级
 func (l *LogLevel) String() string {
 	switch *l {
 	case DEBUG:
@@ -57,6 +67,7 @@ func (l *LogLevel) String() string {
 	return "invalid"
 }
 
+//通过字符串获取日志等级
 func ParseLogLevel(levelstr string) (LogLevel, error) {
 	switch strings.ToLower(levelstr) {
 	case "debug":
@@ -73,13 +84,19 @@ func ParseLogLevel(levelstr string) (LogLevel, error) {
 	return 0, fmt.Errorf("invalid log level '%s' (debug, info, warn, error, fatal)", levelstr)
 }
 
+//日志调用
+//Param1 log对象
+//Param2 配置的级别
+//Param3 当前消息的等级
 func Logf(logger Logger, cfgLevel LogLevel, msgLevel LogLevel, f string, args ...interface{}) {
+	//消息级别没有配置高直接返回
 	if cfgLevel > msgLevel {
 		return
 	}
 	logger.Output(3, fmt.Sprintf(msgLevel.String()+": "+f, args...))
 }
 
+//在没有日志对象的情况下输出严:重错误日志
 func LogFatal(prefix string, f string, args ...interface{}) {
 	logger := log.New(os.Stderr, prefix, log.Ldate|log.Ltime|log.Lmicroseconds)
 	Logf(logger, FATAL, FATAL, f, args...)
