1f756c9854619cd7db19ffc2e91e9edca41e7bc7
xuchaochi <xuchaochi@gmail.com>
2019-09-16 01:46:25 +0800

    [up]http_api

--- internal/http_api/api_request.go
+++ internal/http_api/api_request.go
@@ -14,6 +14,7 @@ import (
 )
 
 // A custom http.Transport with support for deadline timeouts
+//Http客户端支持超时 Transport不是很理解是什么意思
 func NewDeadlineTransport(connectTimeout time.Duration, requestTimeout time.Duration) *http.Transport {
 	// arbitrary values copied from http.DefaultTransport
 	transport := &http.Transport{
@@ -47,35 +48,41 @@ func NewClient(tlsConfig *tls.Config, connectTimeout time.Duration, requestTimeo
 
 // GETV1 is a helper function to perform a V1 HTTP request
 // and parse our NSQ daemon's expected response format, with deadlines.
+//GETV1是V1版本Http请求的解析函数
+//并且格式化返回函数，以及超时控制
 func (c *Client) GETV1(endpoint string, v interface{}) error {
 retry:
+	//新建一个Get请求
 	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
 		return err
 	}
 
 	req.Header.Add("Accept", "application/vnd.nsq; version=1.0")
-
+	//调用client的接收逻辑
 	resp, err := c.c.Do(req)
 	if err != nil {
 		return err
 	}
-
+	//获取返回协议body中的内容
 	body, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 	if err != nil {
 		return err
 	}
+	//判断请求状态是否是成功
 	if resp.StatusCode != 200 {
 		if resp.StatusCode == 403 && !strings.HasPrefix(endpoint, "https") {
+			//403并且请求地址是https的
 			endpoint, err = httpsEndpoint(endpoint, body)
 			if err != nil {
 				return err
-			}
+			} //改变地址后重新尝试
 			goto retry
 		}
 		return fmt.Errorf("got response %s %q", resp.Status, body)
 	}
+	//将body中的内容转换为v
 	err = json.Unmarshal(body, &v)
 	if err != nil {
 		return err
@@ -119,20 +126,22 @@ retry:
 	return nil
 }
 
+//返回最终的https地址和端口https://xxx.xxx.xxx.xxx:xx
 func httpsEndpoint(endpoint string, body []byte) (string, error) {
 	var forbiddenResp struct {
 		HTTPSPort int `json:"https_port"`
 	}
+	//body中的端口
 	err := json.Unmarshal(body, &forbiddenResp)
 	if err != nil {
 		return "", err
 	}
-
+	//获取url
 	u, err := url.Parse(endpoint)
 	if err != nil {
 		return "", err
 	}
-
+	//获取url中的主机地址
 	host, _, err := net.SplitHostPort(u.Host)
 	if err != nil {
 		return "", err
--- internal/http_api/http_server.go
+++ internal/http_api/http_server.go
@@ -19,14 +19,20 @@ func (l logWriter) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
+//Param1监听对象
+//Param2反射函数
+//Param3协议类型
+//Param4日志函数
 func Serve(listener net.Listener, handler http.Handler, proto string, logf lg.AppLogFunc) error {
 	logf(lg.INFO, "%s: listening on %s", proto, listener.Addr())
-
+	//创建HTTP对象
 	server := &http.Server{
-		Handler:  handler,
+		Handler: handler,
+		//错误日志
 		ErrorLog: log.New(logWriter{logf}, "", 0),
 	}
 	err := server.Serve(listener)
+	// 排除正常关闭的情况
 	// theres no direct way to detect this error because it is not exposed
 	if err != nil && !strings.Contains(err.Error(), "use of closed network connection") {
 		return fmt.Errorf("http.Serve() error - %s", err)
