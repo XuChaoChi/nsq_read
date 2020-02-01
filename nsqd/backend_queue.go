9e289bab97dd70c34fd10de8037d3cf4e2dd46d1
ChiXuChao <xuchaochi@gmail.com>
2020-02-02 03:16:10 +0800

    阅读backend_queue.go

--- nsqd/backend_queue.go
+++ nsqd/backend_queue.go
@@ -2,11 +2,12 @@ package nsqd
 
 // BackendQueue represents the behavior for the secondary message
 // storage system
+//消息(存储)队列的接口定义
 type BackendQueue interface {
-	Put([]byte) error
-	ReadChan() chan []byte // this is expected to be an *unbuffered* channel
-	Close() error
-	Delete() error
-	Depth() int64
-	Empty() error
+	Put([]byte) error	//推入消息
+	ReadChan() chan []byte // this is expected to be an *unbuffered* channel //读取消息的channel
+	Close() error	//关闭队列
+	Delete() error	//删除队列
+	Depth() int64	//队列深度
+	Empty() error	//清空队列
 }
